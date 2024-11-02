be2aa58d246eb8c5184f5ce259fcb265be55895a
Abdul Basit <2017n0856@gmail.com>
2024-11-02 22:15:01 +0500

    env configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,17 +4,19 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientsModule } from './modules/client/client.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // This ensures that config is available globally
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
       port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-      // username: process.env.DATABASE_USER,
-      username: 'admin',
-      // password: process.env.DATABASE_PASSWORD,
-      password: 'admin',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
       database: 'jobsync',
       autoLoadEntities: true,
       synchronize: true,
