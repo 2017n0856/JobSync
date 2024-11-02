37b73e401801b646462889f03daccdf0aa2ac113
Abdul Basit <2017n0856@gmail.com>
2024-11-02 22:06:00 +0500

    removed controller for REST apis

--- backend/src/modules/client/client.module.ts
+++ backend/src/modules/client/client.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientsService } from 'src/services/client/client.service';
-import { ClientsController } from 'src/controllers/client/client.controller';
 import { Client } from 'src/entities/client/client.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientsResolver } from 'src/resolvers/client/client.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Client])],
-  controllers: [ClientsController],
+  controllers: [],
   providers: [ClientsResolver, ClientsService],
 })
 export class ClientsModule {}
